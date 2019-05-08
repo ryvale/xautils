83d880012ca9e46bb82d7ce9c15afe2851ca1936
ryvale <valenother@gmail.com>
2019-05-08 17:27:32 +0000

    Report uri enhanced

--- src/main/java/com/exa/utils/io/FilesRepositories.java
+++ src/main/java/com/exa/utils/io/FilesRepositories.java
@@ -6,8 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FilesRepositories {
+	public final static String REP_DEFAULT = "default";
+	
 	private Map<String, RepoPart> repoParts = new HashMap<>();
 	
+	private String defaultRep;
+	
+	public FilesRepositories(String defaultRep) {
+		super();
+		this.defaultRep = defaultRep;
+	}
+	
+	public FilesRepositories() { this(REP_DEFAULT); }
+
 	public void addRepoPart(String name, RepoPart part) {
 		repoParts.put(name, part);
 	}
@@ -15,17 +26,37 @@ public class FilesRepositories {
 	public InputStream getInputStream(String uri) throws IOException {
 		String parts[]  = uri.split("[:]");
 		
-		RepoPart rp = repoParts.get(parts[0]);
+		String repName; String resName;
+		if(parts.length == 1) {
+			repName = defaultRep;
+			resName = uri;
+		}
+		else {
+			repName = parts[0];
+			resName = uri.substring(parts[0].length()+1);
+		}
 		
-		return rp.getInputStream(uri.substring(parts[0].length()));
+		RepoPart rp = repoParts.get(repName);
+		
+		return rp.getInputStream(resName);
 	}
 	
 	
 	public String getName(String uri) {
 		String parts[]  = uri.split("[:]");
 		
-		RepoPart rp = repoParts.get(parts[0]);
+		String repName; String resName;
+		if(parts.length == 1) {
+			repName = defaultRep;
+			resName = uri;
+		}
+		else {
+			repName = parts[0];
+			resName = uri.substring(parts[0].length()+1);
+		}
 		
-		return rp.getName(uri.substring(parts[0].length()+1));
+		RepoPart rp = repoParts.get(repName);
+		
+		return rp.getName(resName);
 	}
 }
